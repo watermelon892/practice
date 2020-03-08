afbb3ea25b314fcdb602175235b6df3c8f4f6e89
watermelon892 <sato.taro.892@gmail.com>
2020-03-09 00:08:42 +0900

    Add a fizzbuzz in Python

---
+++ Python/FizzBuzz/fizzbuzz.py
@@ -0,0 +1,11 @@
+i = 1
+while i <= 15 :
+    if i % 15 == 0 :
+        print('FizzBuzz')
+    elif i % 5 == 0 :
+        print('Fizz')
+    elif i % 3 == 0 :
+        print('Buzz')
+    else :
+        print(i)
+    i += 1
