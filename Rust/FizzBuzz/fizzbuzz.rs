a322cf0b21b6c138507e45236edbe93870a68c82
watermelon892 <sato.taro.892@gmail.com>
2020-03-10 00:06:21 +0900

    Add a fizzbuzz in Rust

---
+++ Rust/FizzBuzz/fizzbuzz.rs
@@ -0,0 +1,19 @@
+fn fizzbuzz(n: i32) {
+    let mut i = 1;
+    while i <= n {
+        if i % 15 == 0 {
+            println!("FizzBuzz");
+        } else if i % 5 == 0 {
+            println!("Fizz");
+        } else if i % 3 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", i)
+        }
+        i += 1;
+    }
+}
+
+fn main() {
+    fizzbuzz(15);
+}
