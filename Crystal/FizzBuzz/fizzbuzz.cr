04e89f0744deea8d0bde2ca06d62f08a7af601ea
watermelon892 <sato.taro.892@gmail.com>
2020-03-25 00:05:17 +0900

    Add a fizzbuzz in Crystal

---
+++ Crystal/FizzBuzz/fizzbuzz.cr
@@ -0,0 +1,17 @@
+def fizzbuzz(n)
+  i = 1
+  while i <= n
+    if i % 15 == 0
+      puts "FizzBuzz"
+    elsif i % 3 == 0
+      puts "Fizz"
+    elsif i % 5 == 0
+      puts "Buzz"
+    else
+      puts i
+    end
+    i += 1
+  end
+end
+
+fizzbuzz(15)
