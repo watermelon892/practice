0df5449c429a2cde70971dcb009ed2832673f73b
watermelon892 <sato.taro.892@gmail.com>
2020-05-19 00:02:39 +0900

    Add a fizzbuzz in Objective-C

---
+++ Objective-C/FizzBuzz/fizzbuzz.m
@@ -0,0 +1,15 @@
+#import <stdio.h>
+
+void main(void) {
+  for (int i = 1; i <= 15; i++) {
+    if (i % 15 == 0) {
+      printf("FizzBuzz\n");
+    } else if (i % 3 == 0) {
+      printf("Fizz\n");
+    } else if (i % 5 == 0) {
+      printf("Buzz\n");
+    } else {
+      printf("%d\n", i);
+    }
+  }
+}
