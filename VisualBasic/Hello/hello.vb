4122298be6af5a5932a23d0c5cfaf9cc73628ae1
sato <sato.taro.892@gmail.com>
2020-10-21 00:01:48 +0900

    Add a hello in VisualBasic

---
+++ VisualBasic/Hello/hello.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Public Class Hanoi
+  Public Shared Sub Main()
+    Dim name As String
+    name = Console.ReadLine()
+    Console.WriteLine("Hello, " & name)
+  End Sub
+End Class
