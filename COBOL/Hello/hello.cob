dadbbdc158e01d2e552ae61f03ef4dd4b4f570ef
sato <sato.taro.892@gmail.com>
2020-11-06 00:01:46 +0900

    Add a hello in COBOL

---
+++ COBOL/Hello/hello.cob
@@ -0,0 +1,11 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.    HELLO.
+
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+
+       PROCEDURE  DIVISION.
+           DISPLAY "Hello, world!"
+           STOP RUN.
