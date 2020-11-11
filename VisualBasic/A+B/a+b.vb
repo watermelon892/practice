00988c9df7401007e0b69c43706288acfc043494
sato <sato.taro.892@gmail.com>
2020-11-12 00:01:51 +0900

    Add a A+B in VisualBasic

---
+++ VisualBasic/A+B/a+b.vb
@@ -0,0 +1,11 @@
+Imports System
+
+Public Class AddAB
+  Public Shared Sub Main()
+    Dim a As Integer
+    Dim b As Integer
+    a = Console.ReadLine()
+    b = Console.ReadLine()
+    Console.WriteLine(a + b)
+  End Sub
+End Class
