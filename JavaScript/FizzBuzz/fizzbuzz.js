cf4effae8eab193eb8a8714837d272bd06ecfc4e
sato <sato.taro.892@gmail.com>
2020-11-23 00:01:58 +0900

    Fixed a FizzBuzz in JavaScript

--- JavaScript/FizzBuzz/fizzbuzz.js
+++ JavaScript/FizzBuzz/fizzbuzz.js
@@ -1,16 +1,18 @@
-function fizzbuzz(i) {
-    let result;
-    if(i % 15 === 0) result = "FizzBuzz";
-    else if (i % 3 === 0) result = "Fizz";
-    else if (i % 5 === 0) result = "Buzz";
-    else result = i;
-    return result;
-}
+const fizzbuzz = (i) => {
+    if(i % 15 === 0) {
+        return "FizzBuzz";
+    } else if (i % 3 === 0) {
+        return "Fizz";
+    } else if (i % 5 === 0) {
+        return "Buzz";
+    }
+    return i;
+};
 
-function run(n) {
+const run = (n) => {
     for(let i = 1; i <= n; i++) {
         console.log(fizzbuzz(i));
     }
-}
+};
 
 run(15);
